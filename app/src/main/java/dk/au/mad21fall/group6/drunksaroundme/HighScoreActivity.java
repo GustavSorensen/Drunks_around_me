edcf8983d04a8a1da7d7a6e36a32fc1c091c6726
GustavSorensen <gustav@fiberflex.dk>
2021-12-04 15:49:58 +0100

    Alle views undtagen MapView er lavet

---
+++ app/src/main/java/dk/au/mad21fall/group6/drunksaroundme/HighScoreActivity.java
@@ -0,0 +1,15 @@
+
+package dk.au.mad21fall.group6.drunksaroundme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HighScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_score);
+    }
+}
---
+++ app/src/main/java/dk/au/mad21fall/group6/drunksaroundme/HostActivity.java
@@ -0,0 +1,14 @@
+package dk.au.mad21fall.group6.drunksaroundme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host);
+    }
+}
